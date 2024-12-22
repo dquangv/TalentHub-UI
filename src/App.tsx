1e8b2c9ee6f320f7a30635853ffd2d402f0fc121
tiendinh203 <tientc0909@gmail.com>
2024-12-22 10:45:12 +0700

    Remove about page and contact page
    Remove about page and contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
 import Home from '@/pages/Home';
-import About from '@/pages/About';
-import Contact from '@/pages/Contact';
+// import About from '@/pages/About';
+// import Contact from '@/pages/Contact';
 import NotFound from '@/pages/NotFound';
 
 function App() {
@@ -16,8 +16,8 @@ function App() {
           <main>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
+              {/* <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} /> */}
               <Route path="*" element={<div style={{width: '777px', margin: '0 auto'}}><NotFound /></div>} />
             </Routes>
           </main>
