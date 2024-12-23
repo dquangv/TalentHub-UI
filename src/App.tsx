20420edcd0c040c4ce2ef3de02194f0f27aab11d
tiendinh203 <tientc0909@gmail.com>
2024-12-23 19:38:48 +0700

    update router
    update router

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import Contact from '@/pages/Contact';
 import NotFound from '@/pages/NotFound';
 import ScrollToTop from './components/layout/ScrollToTop';
 import Freelancers from './pages/Freelancers';
-
-import Jobs from '@/pages/Jobs';
-import Login from '@/pages/auth/Login';
-import Register from '@/pages/auth/Register';
+import Jobs from './pages/Jobs';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Settings from './pages/freelancer/settings/Settings';
 
 function App() {
   return (
@@ -26,8 +26,10 @@ function App() {
               <Route path="/jobs" element={<Jobs />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/login" element={<Login />} /> 
+              <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/settingsfreelancer" element={<Settings />} />
+
               <Route path="*" element={<div style={{width: '50%', margin: '0 auto'}}><NotFound /></div>} />
             </Routes>
           </main>
