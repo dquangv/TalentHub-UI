592a5d918b3da9c06569365b603e55969ab44998
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-12-25 14:35:51 +0700

    Add Flag Language & Ignore locales folder

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -15,7 +15,7 @@ export function LanguageToggle() {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" size="icon" className="h-8 w-8">
-                    <Languages className="h-4 w-4" />
+                    <Languages className=" w-4" />
                     <span className="sr-only">Toggle language</span>
                 </Button>
             </DropdownMenuTrigger>
@@ -24,33 +24,59 @@ export function LanguageToggle() {
                     onClick={() => setLanguage('vi')}
                     className={language === 'vi' ? 'bg-primary-50' : ''}
                 >
+                    <img
+                        src="https://flagcdn.com/w40/vn.png"
+                        alt="Vietnam Flag"
+                        className="w-4 h-4 mr-2"
+                    />
                     Tiếng Việt
                 </DropdownMenuItem>
                 <DropdownMenuItem
                     onClick={() => setLanguage('en')}
                     className={language === 'en' ? 'bg-primary-50' : ''}
                 >
+                    <img
+                        src="https://flagcdn.com/w40/us.png"
+                        alt="English Flag"
+                        className="w-4  mr-2"
+                    />
                     English
                 </DropdownMenuItem>
                 <DropdownMenuItem
                     onClick={() => setLanguage('ko')}
                     className={language === 'ko' ? 'bg-primary-50' : ''}
                 >
+                    <img
+                        src="https://flagcdn.com/w40/kr.png"
+                        alt="Korean Flag"
+                        className="w-4  mr-2"
+                    />
                     한국인
                 </DropdownMenuItem>
                 <DropdownMenuItem
                     onClick={() => setLanguage('ja')}
                     className={language === 'ja' ? 'bg-primary-50' : ''}
                 >
+                    <img
+                        src="https://flagcdn.com/w40/jp.png"
+                        alt="Japanese Flag"
+                        className="w-4  mr-2"
+                    />
                     日本語
                 </DropdownMenuItem>
                 <DropdownMenuItem
                     onClick={() => setLanguage('zhcn')}
                     className={language === 'zhcn' ? 'bg-primary-50' : ''}
                 >
+                    <img
+                        src="https://flagcdn.com/w40/cn.png"
+                        alt="Chinese Flag"
+                        className="w-4  mr-2"
+                    />
                     中国人
                 </DropdownMenuItem>
             </DropdownMenuContent>
+
         </DropdownMenu>
     );
 }
