abc1e3552a326e9f4b7fc44a866cd073c25819d2
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-01-06 13:06:10 +0700

    Ok

---
+++ src/routes/index.tsx
@@ -0,0 +1,59 @@
+import { RouteObject } from 'react-router-dom';
+import Home from '@/pages/Home';
+import About from '@/pages/About';
+import Contact from '@/pages/Contact';
+import NotFound from '@/pages/NotFound';
+import Freelancers from '@/pages/Freelancers';
+import FreelancerDetail from '@/pages/freelancers/FreelancerDetail';
+import Jobs from '@/pages/Jobs';
+import Login from '@/pages/auth/Login';
+import Register from '@/pages/auth/Register';
+import Settings from '@/pages/freelancer/settings/Settings';
+import Pricing from '@/pages/Pricing';
+
+export const mainRoutes: RouteObject[] = [
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/freelancers',
+        element: <Freelancers />
+    },
+    {
+        path: '/freelancers/:id',
+        element: <FreelancerDetail />
+    },
+    {
+        path: '/jobs',
+        element: <Jobs />
+    },
+    {
+        path: '/about',
+        element: <About />
+    },
+    {
+        path: '/contact',
+        element: <Contact />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/register',
+        element: <Register />
+    },
+    {
+        path: '/settingsfreelancer',
+        element: <Settings />
+    },
+    {
+        path: '/pricing',
+        element: <Pricing />
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    }
+];
