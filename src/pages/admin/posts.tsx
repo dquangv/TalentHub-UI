ba6f25b24ad48f190082e82159ca31fe8718ef95
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-13 11:27:40 +0700

    Update posts.tsx

--- src/pages/admin/posts.tsx
+++ src/pages/admin/posts.tsx
@@ -2,28 +2,76 @@ import { DataTable } from "@/components/admin/data-table/data-table";
 import { postColumns } from "@/components/admin/data-table/columns";
 import { useEffect, useState } from "react";
 import api from "@/api/axiosConfig";
+import { Button } from "antd";
 
 export default function PostsPage() {
   const [jobs, setJobs] = useState<any[]>([]);
-
-  useEffect(() => {
-    async function fetchJobs() {
-      const response = await api.get("/v1/jobs/admin");
-      console.log("response ", response.data)
-      if (response.status === 200) {
-        
-        setJobs(response.data);
-      }
+  async function fetchJobs() {
+    const response = await api.get("/v1/jobs/admin");
+    console.log("response ", response.data)
+    if (response.status === 200) {
+      
+      setJobs(response.data);
     }
+  }
+  useEffect(() => {
+  
 
     fetchJobs();
   }, []);
+  
+  const handleBan = async (jobId) => {
+    try {
+      await api.post(`/v1/jobs/admin/ban?jobId=${jobId}`);
+      fetchJobs()
+    } catch (error) {
+    }
+  };
+
+  const handleUnban = async (jobId) => {
+    try {
+      await api.post(`/v1/jobs/admin/unban?jobId=${jobId}`);
+      fetchJobs()
+    } catch (error) {
+    }
+  };
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-bold tracking-tight">Quản lý Bài viết</h2>
       </div>
-      <DataTable columns={postColumns} data={jobs} />
+      <DataTable  columns={[
+            ...postColumns,
+            {
+              id: "actions",
+              header: "Actions",
+              cell: ({ row }) => {
+                const id = row.getValue("id");
+                const isBanned = (row.getValue("status") == "Bị cấm");
+                return (
+                  <div className="flex space-x-2">
+                    {isBanned ? (
+                      <Button
+                        onClick={() => handleUnban(id)}
+                        variant="outline"
+                        className="text-green-600"
+                      >
+                        Mở khóa
+                      </Button>
+                    ) : (
+                      <Button
+                        onClick={() => handleBan(id)}
+                        variant="outline"
+                        className="text-red-600"
+                      >
+                        Khóa
+                      </Button>
+                    )}
+                  </div>
+                );
+              },
+            },
+          ]} data={jobs}/>
     </div>
   );
 }
