d68b79ce6a0d156f0ca5f4f7169e3aec0b85580f
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-03-19 22:01:35 +0700

    Wrap <App/> by MessageProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { LanguageProvider } from './contexts/LanguageContext';
 import App from './App.tsx';
 import './index.css';
+import { MessageProvider } from './pages/ChatComponents/MessageContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <LanguageProvider>
+  <LanguageProvider>
+    <MessageProvider>
       <App />
-    </LanguageProvider>
+    </MessageProvider>
+  </LanguageProvider>
 );
