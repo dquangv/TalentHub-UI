6ee1504d26d5c67c51eb7f4178f4baba807a8dc9
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-21 10:07:41 +0700

    Update DegreesPage.tsx

--- src/pages/admin/DegreesPage.tsx
+++ src/pages/admin/DegreesPage.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { notification } from "antd";
+import { notification, Modal } from "antd";
 import api from "@/api/axiosConfig";
 
 const schoolColumns = [
@@ -26,19 +26,18 @@ const schoolColumns = [
   {
     accessorKey: "quantityEducation",
     header: "Số lượng người dùng",
-  }
-  
-  
+  },
 ];
 
 export function DegreesPage() {
   const [data, setData] = useState<any[]>([]);
   const [formData, setFormData] = useState({ degreeTitle: "" });
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [editingSchool, setEditingSchool] = useState<any | null>(null);
+  const [editingDegree, setEditingDegree] = useState<any | null>(null);
+  const [filterValue, setFilterValue] = useState<string>("");
 
   useEffect(() => {
-    const fetchdegrees = async () => {
+    const fetchDegrees = async () => {
       try {
         const response = await api.get("/v1/degrees");
         setData(response.data);
@@ -49,7 +48,7 @@ export function DegreesPage() {
         });
       }
     };
-    fetchdegrees();
+    fetchDegrees();
   }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -61,8 +60,8 @@ export function DegreesPage() {
 
     try {
       let response;
-      if (editingSchool) {
-        response = await api.put(`/v1/degrees/${editingSchool.id}`, formData);
+      if (editingDegree) {
+        response = await api.put(`/v1/degrees/${editingDegree.id}`, formData);
         notification.success({ message: "Cập nhật thành công", description: "Bằng cấp học đã được cập nhật." });
       } else {
         response = await api.post("/v1/degrees", formData);
@@ -70,9 +69,9 @@ export function DegreesPage() {
       }
 
       setData((prevData) => {
-        if (editingSchool) {
-          return prevData.map((school) =>
-            school.id === editingSchool.id ? response.data : school
+        if (editingDegree) {
+          return prevData.map((degree) =>
+            degree.id === editingDegree.id ? response.data : degree
           );
         } else {
           return [response.data, ...prevData];
@@ -80,7 +79,7 @@ export function DegreesPage() {
       });
 
       setDialogOpen(false);
-      setEditingSchool(null);
+      setEditingDegree(null);
       setFormData({ degreeTitle: "" });
     } catch (error) {
       notification.error({
@@ -90,32 +89,54 @@ export function DegreesPage() {
     }
   };
 
-  const handleEdit = (school: any) => {
-    setEditingSchool(school);
-    setFormData({ degreeTitle: school.degreeTitle });
+  const handleEdit = (degree: any) => {
+    setEditingDegree(degree);
+    setFormData({ degreeTitle: degree.degreeTitle });
     setDialogOpen(true);
   };
 
-  const handleDelete = async (schoolId: string) => {
-    try {
-      await api.delete(`/v1/degrees/${schoolId}`);
-      setData((prevData) => prevData.filter((school) => school.id !== schoolId));
-      notification.success({ message: "Xóa thành công", description: "Bằng cấp học đã bị xóa." });
-    } catch (error) {
-      notification.error({ message: "Lỗi khi xóa", description: "Không thể xóa bằng cấp học này." });
+  const handleDelete = (degreeId: string, quantityEducation: number) => {
+    if (quantityEducation > 0) {
+      Modal.confirm({
+        title: "Xóa bằng cấp",
+        content: "Bằng cấp này có người dùng. Bạn có chắc chắn muốn xóa không?",
+        onOk: async () => {
+          try {
+            await api.delete(`/v1/degrees/${degreeId}`);
+            setData((prevData) => prevData.filter((degree) => degree.id !== degreeId));
+            notification.success({ message: "Xóa thành công", description: "Bằng cấp học đã bị xóa." });
+          } catch (error) {
+            notification.error({ message: "Lỗi khi xóa", description: "Không thể xóa bằng cấp học này." });
+          }
+        },
+      });
+    } else {
+      Modal.confirm({
+        title: "Xóa bằng cấp",
+        content: "Bạn có chắc chắn muốn xóa không?",
+        onOk: async () => {
+          try {
+            await api.delete(`/v1/degrees/${degreeId}`);
+            setData((prevData) => prevData.filter((degree) => degree.id !== degreeId));
+            notification.success({ message: "Xóa thành công", description: "Bằng cấp học đã bị xóa." });
+          } catch (error) {
+            notification.error({ message: "Lỗi khi xóa", description: "Không thể xóa bằng cấp học này." });
+          }
+        },
+      });
     }
   };
 
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
-        <h2 className="text-3xl font-bold tracking-tight">Quản lý bằng cấp Học</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Quản lý Bằng Cấp Học</h2>
         <Dialog
           open={dialogOpen}
           onOpenChange={(open) => {
             setDialogOpen(open);
             if (!open) {
-              setEditingSchool(null);
+              setEditingDegree(null);
               setFormData({ degreeTitle: "" });
             }
           }}
@@ -123,13 +144,13 @@ export function DegreesPage() {
           <DialogTrigger asChild>
             <Button className="flex items-center gap-2">
               <PlusCircle className="h-4 w-4" />
-              {editingSchool ? "Chỉnh sửa bằng cấp" : "Thêm bằng cấp"}
+              {editingDegree ? "Chỉnh sửa bằng cấp" : "Thêm bằng cấp"}
             </Button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-[500px]">
             <DialogHeader>
               <DialogTitle>
-                {editingSchool ? "Chỉnh sửa bằng cấp" : "Thêm bằng cấp Mới"}
+                {editingDegree ? "Chỉnh sửa bằng cấp" : "Thêm bằng cấp Mới"}
               </DialogTitle>
             </DialogHeader>
             <form className="space-y-6" onSubmit={handleSubmit}>
@@ -148,7 +169,7 @@ export function DegreesPage() {
                   Hủy
                 </Button>
                 <Button type="submit">
-                  {editingSchool ? "Lưu thay đổi" : "Lưu"}
+                  {editingDegree ? "Lưu thay đổi" : "Lưu"}
                 </Button>
               </div>
             </form>
@@ -156,34 +177,43 @@ export function DegreesPage() {
         </Dialog>
       </div>
 
-      <DataTable columns={[
-                ...schoolColumns,
-                {
-                  id: "actions",
-                  header: "Actions",
-                  cell: ({ row }) => {
-                    const id = row.getValue("id");
-                    return (
-                      <div className="flex space-x-2">
-                        <Button
-                          onClick={() => handleEdit(row.original)} 
-                          variant="outline"
-                          className="text-blue-600"
-                        >
-                          Chỉnh sửa
-                        </Button>
-                        <Button
-                          onClick={() => handleDelete(id)} 
-                          variant="outline"
-                          className="text-red-600"
-                        >
-                          Xóa
-                        </Button>
-                      </div>
-                    );
-                  },
-                },
-              ]} data={data} />
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Tìm kiếm..."
+          className="max-w-sm"
+          value={filterValue}
+          onChange={(e) => setFilterValue(e.target.value)}
+        />
+      </div>
+
+      <DataTable
+        columns={[
+          ...schoolColumns,
+          {
+            id: "actions",
+            header: "Actions",
+            cell: ({ row }) => {
+              const id = row.getValue("id");
+              const quantityEducation = row.getValue("quantityEducation");
+              return (
+                <div className="flex space-x-2">
+                  <Button onClick={() => handleEdit(row.original)} variant="outline" className="text-blue-600">
+                    Chỉnh sửa
+                  </Button>
+                  <Button
+                    onClick={() => handleDelete(id, quantityEducation)}
+                    variant="outline"
+                    className="text-red-600"
+                  >
+                    Xóa
+                  </Button>
+                </div>
+              );
+            },
+          },
+        ]}
+        data={data.filter((degree) => degree.degreeTitle.toLowerCase().includes(filterValue.toLowerCase()))}
+      />
     </div>
   );
 }
