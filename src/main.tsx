246741686580993fd2f7a514f757cdea4cdcebb1
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-03-27 13:50:01 +0700

    Add  & Import css file for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { LanguageProvider } from './contexts/LanguageContext';
 import App from './App.tsx';
 import './index.css';
+import './mobile-fixes.css';
 import { MessageProvider } from './pages/ChatComponents/MessageContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
