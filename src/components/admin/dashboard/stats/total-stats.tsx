0987bd7368c99abc20b5ddbe1e65d62806ddd939
dquangvu <dquangvu7998@gmail.com>
2025-03-31 18:27:44 +0700

    edit content statics (admin)

--- src/components/admin/dashboard/stats/total-stats.tsx
+++ src/components/admin/dashboard/stats/total-stats.tsx
@@ -6,7 +6,7 @@ export function TotalStats() {
     <div className="grid gap-4 md:grid-cols-2">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Tổng Người Dùng</CardTitle>
+          <CardTitle className="text-sm font-medium">Tổng người dùng</CardTitle>
           <Users className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
@@ -19,7 +19,7 @@ export function TotalStats() {
 
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Tổng Bài Viết</CardTitle>
+          <CardTitle className="text-sm font-medium">Tổng doanh thu</CardTitle>
           <FileText className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
