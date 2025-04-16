23cb592808e06d0a9c3f45f9d6c870df1b48807a
tiendinh203 <tientc0909@gmail.com>
2025-04-16 11:10:09 +0700

    fix full width for tìm kỹ năng

--- src/pages/client/PostJob.tsx
+++ src/pages/client/PostJob.tsx
@@ -615,7 +615,7 @@ const PostJob = () => {
                                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                                 </Button>
                             </PopoverTrigger>
-                            <PopoverContent className="w-full p-0">
+                            <PopoverContent className="w-full p-0 w-[var(--radix-popover-trigger-width)] p-0" >
                                 <Command>
                                     <CommandInput
                                         placeholder="Tìm kỹ năng..."
